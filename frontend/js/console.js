fe5f2f35d6203f0835dec890f216bf43d8e31157
severinRobert <64019051+severinRobert@users.noreply.github.com>
2020-05-08 14:45:25 +0200

    Update console.js

--- frontend/js/console.js
+++ frontend/js/console.js
@@ -1,21 +1,23 @@
-document.addEventListener('DOMContentLoaded',initPage);
-function initPage (){
-	document.getElementById('formulaireConsole').addEventListener('submit',traitementFormulaire);
+function initPage() {
+	id("formulaireConsole").addEventListener('submit',traitementFormulaire);
+}
+
+function traitementFormulaire(event) {
+	event.preventDefault();
+	let form = event.target;
+	let pMarque = form.marque.value;
+	let pAnnee = form.annee.value;
+	let pPortabilite = form.portabilite.value;
+	let xhr = new XMLHttpRequest();   
+	xhr.open('get', "tableConsoleHtml?pMarque=" + pMarque + "&pAnnee=" + pAnnee + "&pPortabilite" + pPortabilite + '', true);
+	xhr.onload = () => {
+		let rTableConsole = JSON.parse(xhr.responseText);
+	}
+	xhr.send();
 }
 
 
-function traitementFormulaire (event){
-event.preventDefault();
-let form = event.target;
-let pMarque = form.marque.value;
-let pAnnee = form.annee.value;
-let pPortabilite = form.portabilite.value;
-/*console.log(form,pMarque,pAnnee,pPortabilite);*/
-let xhr = new XMLHttpRequest();
-xhr.open('get','tableConsoleHtml?pMarque=' + pMarque + '&pAnnee=' + pAnnee + '&pPortabilite=' + pPortabilite + '',true);
-xhr.onload = () => { console.log(xhr)
-	let rTableConsole = JSON.parse(xhr.responseText);
-	console.log(rTableConsole)
+function id(id) {
+	return document.getElementById(id);
 }
-xhr.send()
-}
+
