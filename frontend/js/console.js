28152547fa03d0b993c4cc4eeedceb748eff8afa
EmilienPerremans <64018946+EmilienPerremans@users.noreply.github.com>
2020-05-08 14:25:36 +0200

    Update console.js

--- frontend/js/console.js
+++ frontend/js/console.js
@@ -1,1 +1,21 @@
-"use strict";
+document.addEventListener('DOMContentLoaded',initPage);
+function initPage (){
+	document.getElementById('formulaireConsole').addEventListener('submit',traitementFormulaire);
+}
+
+
+function traitementFormulaire (event){
+event.preventDefault();
+let form = event.target;
+let pMarque = form.marque.value;
+let pAnnee = form.annee.value;
+let pPortabilite = form.portabilite.value;
+/*console.log(form,pMarque,pAnnee,pPortabilite);*/
+let xhr = new XMLHttpRequest();
+xhr.open('get','tableConsoleHtml?pMarque=' + pMarque + '&pAnnee=' + pAnnee + '&pPortabilite=' + pPortabilite + '',true);
+xhr.onload = () => { console.log(xhr)
+	let rTableConsole = JSON.parse(xhr.responseText);
+	console.log(rTableConsole)
+}
+xhr.send()
+}
