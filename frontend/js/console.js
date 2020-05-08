693effc7f9e9e7601ae66767c313fe0e7f5956fb
severinRobert <64019051+severinRobert@users.noreply.github.com>
2020-05-08 15:41:42 +0200

    Update console.js

--- frontend/js/console.js
+++ frontend/js/console.js
@@ -10,11 +10,12 @@ function traitementFormulaire(event) {
 	let form = event.target;
 	let pMarque = form.marque.value;
 	let pAnnee = form.annee.value;
-	let pPortabilite = form.portabilite.value;
+	let pPortabilite = form.portable.value;
 	let xhr = new XMLHttpRequest();   
 	xhr.open('get', "tableConsoleHtml?pMarque=" + pMarque + "&pAnnee=" + pAnnee + "&pPortabilite" + pPortabilite + '', true);
-	xhr.onload = () => {
+	xhr.onload = () => { console.log(xhr.responseText);
 		let rTableConsole = JSON.parse(xhr.responseText);
+		console.log(rTableConsole);
 	}
 	xhr.send();
 }
