9e4d56c617e84865828c37fd406effe6c5b6a12e
severinRobert <64019051+severinRobert@users.noreply.github.com>
2020-05-09 14:01:39 +0200

    Add files via upload

---
+++ backend/scripts-api-rest/severinRobert-webservice-ajout.sql
@@ -0,0 +1,35 @@
+ALTER PROCEDURE "DBA"."ajoutConsole"( IN pMarque VARCHAR (100), IN pAnnee INTEGER, IN pPortabilite VARCHAR (40), in pModele VARCHAR (100) )
+
+BEGIN
+    DECLARE pIdPortabilite INTEGER;
+    DECLARE pIdMarque INTEGER;
+
+    IF pPortable = 'fixe'
+            SET pIdPortabilite = 1;
+    IF pPortable = 'portable'
+            SET pIdPortabilite = 2;
+
+    IF pMarque = 'Nintendo' 
+            SET pIdMarque = 1;
+    IF pMarque = 'Sony'
+            SET pIdMarque = 2;
+    IF pMarque = 'Microsoft'
+            SET pIdMarque = 3;
+    IF pMarque = 'Atari'
+            SET pIdMarque = 4;
+    ELSE
+        INSERT INTO marques
+            (idMarque, nomMarque)
+            VALUES 
+            (5,pMarque);
+
+    INSERT INTO consoles
+        (idPortabilite, idMarque, modele, annee)
+        VALUES 
+        (pIdPortabilite,pIdMarque,pModele,pAnnee);
+
+END
+
+/* *********************************************** */
+
+CREATE SERVICE 
