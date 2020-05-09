b2cad3f52c9b6a346db57960546aa5a699d3b779
severinRobert <64019051+severinRobert@users.noreply.github.com>
2020-05-09 22:22:52 +0200

    Update insertion-console.js

--- frontend/js/insertion-console.js
+++ frontend/js/insertion-console.js
@@ -1,17 +1,17 @@
 "use strict";
 
-id("ajoutConsole").addEventListener('submit',traitementAjoutConsole);
+
 function traitementAjoutConsole(event) {
 	event.preventDefault();
 	let form = event.target;
 	let pMarque = form.marque.value;
-	let pAnneeMin = form.anneeMin.value;
-	let pAnneeMax = form.anneeMax.value;
-	let pPortabilite = form.portable.value;console.log(pMarque,pAnneeMin,pAnneeMax,pPortabilite);
-	let xhr = new XMLHttpRequest();
-	xhr.open('get', "tableConsoleHtml?pMarque=" + pMarque + "&pAnneeMin=" + pAnneeMin + "&pAnneeMax=" + pAnneeMax + "&pPortabilite" + pPortabilite + '', true);
-	xhr.onload = () => { 
-		let rTableConsole = JSON.parse(xhr.responseText);
+	let pAnnee = form.annee.value;
+	let pPortabilite = form.portabilite.value;
+	let pModele = form.modele.value; console.log(pModele + " " + pMarque + " " + pAnnee + " " + pPortabilite);
+	let rAjout = new XMLHttpRequest();
+
+	rAjout.open('get', "ajoutConsole?pMarque=" + pMarque + "&pAnnee=" + pAnnee + "&pPortabilite" + pPortabilite + "&pModele" + pModele + '', true);
+	rAjout.onload = () => {
 	}
-	xhr.send();
+	rAjout.send();
 }
