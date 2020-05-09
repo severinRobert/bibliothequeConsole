ce43182672ddc417213b08eee863369fa2479479
severinRobert <64019051+severinRobert@users.noreply.github.com>
2020-05-09 11:30:44 +0200

    Update recherche-console.js

--- frontend/js/recherche-console.js
+++ frontend/js/recherche-console.js
@@ -8,49 +8,49 @@ function initPage (){
 
 function traitementFormulaire (event){
 	event.preventDefault();
-		let form = event.target;
-		let pMarque = form.marque.value;
-		let pAnnee = form.annee.value;
-		let pPortabilite = form.portabilite.value;
+	let form = event.target;
+	let pMarque = form.marque.value;
+	let pAnnee = form.annee.value;
+	let pPortabilite = form.portabilite.value;
 
-		if (pPortabilite == 'tous')
-		{
-			let xhr2 = new XMLHttpRequest();
-			xhr2.open('get' ,`tableConsoleHtmlMarque?pMarque=${pMarque}`,true );
-			xhr2.onload = () => { console.log(xhr2);
-				let rTableConsole = JSON.parse(xhr2.responseText);
-				let table = tableHtml(rTableConsole);
+	if (pPortabilite == 'tous')
+	{
+		let xhr2 = new XMLHttpRequest();
+		xhr2.open('get' ,`tableConsoleHtmlMarque?pMarque=${pMarque}`,true );
+		xhr2.onload = () => { console.log(xhr2);
+			let rTableConsole = JSON.parse(xhr2.responseText);
+			let table = tableHtml(rTableConsole);
 
-				id("result").innerHTML += table;
-			}
-			xhr2.send();
+			id("result").innerHTML += table;
 		}
+		xhr2.send();
+	}
 
-		else
-		 {
-			let xhr = new XMLHttpRequest();
-			xhr.open('get','tableConsoleHtml?pMarque=' + pMarque + '&pAnnee=' + pAnnee + '&pPortabilite=' + pPortabilite + '',true);
-			xhr.onload = () => { console.log(xhr)
-				let rTableConsole = JSON.parse(xhr.responseText);
-				if (rTableConsole.length != 0){
-					console.log(rTableConsole)
-	 				let tableau = tableHtml(rTableConsole);
+	else
+	{
+		let xhr = new XMLHttpRequest();
+		xhr.open('get','tableConsoleHtml?pMarque=' + pMarque + '&pAnnee=' + pAnnee + '&pPortabilite=' + pPortabilite + '',true);
+		xhr.onload = () => { console.log(xhr)
+			let rTableConsole = JSON.parse(xhr.responseText);
+			if (rTableConsole.length != 0){
+				console.log(rTableConsole)
+	 			let tableau = tableHtml(rTableConsole);
 
-					id("result").innerHTML += tableau;
-					id("erreurDate").innerHTML = "";
-				}
+				id("result").innerHTML += tableau;
+				id("erreurDate").innerHTML = "";
+			}
 
-				else {
-					let xhr1 = new XMLHttpRequest();
-					xhr1.open('get' ,`tableConsoleHtmlDate?pMarque=${pMarque}&pPortabilite=${pPortabilite}`,true );
-					xhr1.onload = () => { console.log(xhr1);
-						rTableConsole = JSON.parse(xhr1.responseText);
-						if (rTableConsole.length != 0){
-							console.log(rTableConsole)
-		 					let tableau = tableHtml(rTableConsole);
+			else {
+				let xhr1 = new XMLHttpRequest();
+				xhr1.open('get' ,`tableConsoleHtmlDate?pMarque=${pMarque}&pPortabilite=${pPortabilite}`,true );
+				xhr1.onload = () => { console.log(xhr1);
+					rTableConsole = JSON.parse(xhr1.responseText);
+					if (rTableConsole.length != 0){
+						console.log(rTableConsole)
+		 				let tableau = tableHtml(rTableConsole);
 
-		 					id("result").innerHTML += tableau;
-							id("erreurDate").innerText = 'Champs dâte ignorer!';
+		 				id("result").innerHTML += tableau;
+						id("erreurDate").innerText = 'Champs dâte ignorer!';
 		 			}
 		 			else {
 		 				id("erreurDate").innerText = `La marque ${pMarque} n'as pas de consoles ${pPortabilite}` ;
