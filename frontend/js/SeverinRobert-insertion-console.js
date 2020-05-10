b252a604b5b84b3d3b1f891e1d5162ac0aaa03b4
severinRobert <64019051+severinRobert@users.noreply.github.com>
2020-05-10 21:11:14 +0200

    Update SeverinRobert-insertion-console.js

--- frontend/js/SeverinRobert-insertion-console.js
+++ frontend/js/SeverinRobert-insertion-console.js
@@ -6,7 +6,7 @@
 */
 
 function traitementAjoutConsole(event) {
-	event.preventDefault();
+	event.preventDefault();					//récupération + initialisation de données
 	let form = event.target;
 	let pMarque = form.marque.value;
 	let pAnnee = form.annee.value;
@@ -23,8 +23,7 @@ function traitementAjoutConsole(event) {
 			id("infoAjout").innerHTML += "L'ajout n'a pas pu se faire, la console existe déjà dans notre base de donnée.<br />";
 		} 
 		else {
-			let rMarque = new XMLHttpRequest();				
-			console.log("console OK");
+			let rMarque = new XMLHttpRequest();
 			rMarque.open('get', `verifMarque?pMarque=${pMarque}`, true);		//vérifie si la marque existe déjà dans la DB
 			rMarque.onload = () => {
 
@@ -34,16 +33,17 @@ function traitementAjoutConsole(event) {
 					let rAjoutMarque = new XMLHttpRequest();
 					rAjoutMarque.open('get', `ajoutMarque?pMarque=${pMarque}`,true);
 					rAjoutMarque.onload = () => {
-						ajoutMarqueChoix();
+						ajoutMarqueChoix();     				//ajoute le choix de la marque créée dans le formulaire de recherche
 					}
 					rAjoutMarque.send()
 				} 
+
 				let rIdMarque = new XMLHttpRequest();
 				rIdMarque.open('get', `idMarque?pMarque=${pMarque}` + '',true);
 				rIdMarque.onload = () => {
-					console.log("id OK");
 					pIdMarque = rIdMarque.responseText;
-					let rAjoutConsole = new XMLHttpRequest();
+
+					let rAjoutConsole = new XMLHttpRequest();				//ajoute la console à la DB
 					pIdPortabilite = (pPortabilite == "fixe") ? 1 : 2;
 					rAjoutConsole.open('get',`ajoutConsole?pIdMarque=${pIdMarque}&pAnnee=${pAnnee}&pIdPortabilite=${pIdPortabilite}&pModele=${pModele}`,true)
 					rAjoutConsole.onload = () => {
@@ -59,16 +59,17 @@ function traitementAjoutConsole(event) {
 	rExiste.send()
 }
 
-
+/**
+ * Crée dynamiquement les options de marque pour la recherche dans la DB au chargement de la page et lors de l'ajout d'une marque
+ */
 function ajoutMarqueChoix() {
 	let  tableauMarque = new XMLHttpRequest();
 	tableauMarque.open('get' ,"tableauMarque?",true );
 	tableauMarque.onload = () => {
 		let rTableMarque = JSON.parse(tableauMarque.responseText);
-		console.log(rTableMarque);
 		let tableHtmlMarque = "";
 		for(let i in rTableMarque)	{
-			tableHtmlMarque += `<option value="${rTableMarque[i].nomMarque}">${rTableMarque[i].nomMarque}</option>`
+			tableHtmlMarque += `<option value="${rTableMarque[i].nomMarque}">${rTableMarque[i].nomMarque}</option>`		//creation des options de marque
 		}
 		id("marque").innerHTML = tableHtmlMarque;
 	}
