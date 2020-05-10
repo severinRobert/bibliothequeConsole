6c45ec52e3a13d18dc5273c4d3037abe2ff1096d
ArthurBehets <64364135+ArthurBehets@users.noreply.github.com>
2020-05-10 16:04:46 +0200

    Update and rename triage-console.js to comment-console.js

---
+++ frontend/js/comment-console.js
@@ -0,0 +1,41 @@
+"use strict";
+
+
+function showComment(){
+    let commentXhr = new XMLHttpRequest();
+    commentXhr.open('get', 'tableConsoleComment?', true);
+    commentXhr.onload=() => {
+        let comment = JSON.parse(commentXhr.responseText);
+        comment = affichageComments(comment);
+        id('commentPlace').innerHTML = comment;
+    }
+
+    commentXhr.send();
+}
+
+
+function affichageComments(comment){
+    let affichageComments ="";
+    for (let i in comment){
+        affichageComments += "<p>" + comment[i]['comments'] + "</p>";
+    }
+    return affichageComments;
+}
+
+
+function addComment(event){
+    event.preventDefault();
+    let form = event.target;
+    let newComment = (form.newComment.value);
+    console.log(newComment);
+    let newCommentXhr = new XMLHttpRequest;
+    console.log('ok');
+    newCommentXhr.open('get', `addCommentService?newComment=${newComment}`, true);
+    console.log('ok2');
+    newCommentXhr.onload = () =>{
+
+    }
+    console.log("ok3");
+    newCommentXhr.send();
+    console.log('ok4');
+}
