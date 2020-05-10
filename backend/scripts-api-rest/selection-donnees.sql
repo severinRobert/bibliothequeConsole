f903770d4b75213a3ddf4f02cd9ac7940c646e32
ArthurBehets <64364135+ArthurBehets@users.noreply.github.com>
2020-05-10 22:32:38 +0200

    Delete selection-donnees.sql

--- backend/scripts-api-rest/selection-donnees.sql
+++
@@ -1,6 +0,0 @@
-ALTER PROCEDURE "DBA"."getTableauConsole"(IN pMarque VARCHAR (100), IN pAnneeMin INTEGER, IN pAnneeMax INTEGER, IN pPortabilite VARCHAR (40))
-BEGIN
-    SELECT modele,nomMarque,typePortabilite 
-    FROM consoles NATURAL JOIN marques NATURAL JOIN portabilite
-    WHERE marque.nomMarque = pMarque AND  annee BETWEEN pAnneeMin AND pAnneeMax AND portabilite.typePortabilite = pPortabilite 
-END
