844b3bb1a1d9ee014da9ee8a694f614339c33573
severinRobert <64019051+severinRobert@users.noreply.github.com>
2020-05-10 17:34:16 +0200

    Update EmilienPerremans-recherche-console.js

--- frontend/js/EmilienPerremans-recherche-console.js
+++ frontend/js/EmilienPerremans-recherche-console.js
@@ -1,46 +1,45 @@
-document.addEventListener('DOMContentLoaded',initPage);
-function initPage (){
-	document.getElementById('formulaireConsole').addEventListener('submit',traitementFormulaire);
-}
+"usestrict";
 
+document.addEventListener('DOMContentLoaded',initPage);
+
+function initPage (){
+	id('formulaireConsole').addEventListener('submit',traitementFormulaire);
+	id("ajoutConsole").addEventListener('submit',traitementAjoutConsole);
+}
 
 function traitementFormulaire (event){
 	event.preventDefault();
-		let form = event.target;
-		let pMarque = form.marque.value;
-		let pAnnee = form.annee.value;
-		let pPortabilite = form.portabilite.value;
-/*console.log(form,pMarque,pAnnee,pPortabilite);*/
-		if (pPortabilite == 'tous')
-		{
-			let xhr2 = new XMLHttpRequest();
-			xhr2.open('get' ,`tableConsoleHtmlMarque?pMarque=${pMarque}`,true );
-			xhr2.onload = () => { console.log(xhr2.responseText);
+	let form = event.target;
+	let pMarque = form.marque.value;
+	let pAnnee = form.annee.value;
+	let pPortabilite = form.portabilite.value;
+
+	if (pPortabilite == 'tous')
+	{
+		let xhr2 = new XMLHttpRequest();
+		xhr2.open('get' ,`tableConsoleHtmlMarque?pMarque=${pMarque}`,true );
+		xhr2.onload = () => { console.log(xhr2.responseText);
 			let rTableConsole = JSON.parse(xhr2.responseText);
 			let table = tableHtml(rTableConsole);
 
-	id("result").innerHTML += table;
-			}
-			xhr2.send();
+			id("result").innerHTML += table;
 		}
-
-		else
-		 {
-
-		
-
-
-		let xhr = new XMLHttpRequest();
-xhr.open('get','tableConsoleHtml?pMarque=' + pMarque + '&pAnnee=' + pAnnee + '&pPortabilite=' + pPortabilite + '',true);
-xhr.onload = () => { console.log(xhr)
-	let rTableConsole = JSON.parse(xhr.responseText);
-	if (rTableConsole.length != 0){
-		console.log(rTableConsole)
-	 let tableau = tableHtml(rTableConsole);
-
-	id("result").innerHTML += tableau;
-	id("erreurDate").innerHTML = "";
+		xhr2.send();
 	}
+
+	else
+	{
+		let xhr = new XMLHttpRequest();
+		xhr.open('get','tableConsoleHtml?pMarque=' + pMarque + '&pAnnee=' + pAnnee + '&pPortabilite=' + pPortabilite + '',true);
+		xhr.onload = () => { console.log(xhr)
+			let rTableConsole = JSON.parse(xhr.responseText);
+			if (rTableConsole.length != 0){
+				console.log(rTableConsole)
+	 			let tableau = tableHtml(rTableConsole);
+
+				id("result").innerHTML += tableau;
+				id("erreurDate").innerHTML = "";
+			}
 
 			else {
 				let xhr1 = new XMLHttpRequest();
@@ -48,19 +47,18 @@ xhr.onload = () => { console.log(xhr)
 				xhr1.onload = () => { console.log(xhr1);
 					rTableConsole = JSON.parse(xhr1.responseText);
 					if (rTableConsole.length != 0){
-					console.log(rTableConsole)
+						console.log(rTableConsole)
 		 				let tableau = tableHtml(rTableConsole);
+
 		 				id("result").innerHTML += tableau;
-				id("erreurDate").innerText = 'Champs date ignorer!';
+						id("erreurDate").innerText = 'Champs date ignorer!';
 		 			}
 		 			else {
 		 				id("erreurDate").innerText = `La marque ${pMarque} n'as pas de consoles ${pPortabilite}` ;
 		 			}
 				
 				}
-				
 				xhr1.send()
-
 			}
 		}	
 	xhr.send()
@@ -69,20 +67,11 @@ xhr.onload = () => { console.log(xhr)
 
 function tableHtml (array){
 	let tableau = "" ;
-		array.forEach (element=>{
-			if (!id(element.idConsole)){
-				tableau+=` <tr id="${element.idConsole}"> <td class="${element.nomMarque}">${element.nomMarque}</td>    <td id="${element.modele}">${element.modele}</td>     <td class="${element.annee}">${element.annee}</td>      <td class="${element.typePortabilite}">${element.typePortabilite}</td> </tr> `
-			}
-			
-		});
-		return tableau;
+
+	array.forEach (element=>{
+		if (!id(element.idConsole)){
+			tableau+=` <tr id="${element.idConsole}"> <td class="${element.nomMarque}">${element.nomMarque}</td>    <td id="${element.modele}">${element.modele}</td>     <td class="${element.annee}">${element.annee}</td>      <td class="${element.typePortabilite}">${element.typePortabilite}</td> </tr> `
+		}			
+	});
+	return tableau;
 }
-
-
-
-function id (idDom){
-	return document.getElementById(idDom)
-}
-
-
-
