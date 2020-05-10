1a8b0943ca3e683a2d0280c5297bb79daec81177
severinRobert <64019051+severinRobert@users.noreply.github.com>
2020-05-10 16:13:04 +0200

    Update severinRobert-webservice-ajout.sql

--- backend/scripts-api-rest/severinRobert-webservice-ajout.sql
+++ backend/scripts-api-rest/severinRobert-webservice-ajout.sql
@@ -1,31 +1,45 @@
-CREATE  PROCEDURE "DBA"."ajoutConsole"( IN pMarque VARCHAR (100), IN pAnnee INTEGER, IN pPortabilite VARCHAR (40), in pModele VARCHAR (100) )
-
+CREATE PROCEDURE "DBA"."ajoutConsole"( IN pIdMarque INTEGER, IN pAnnee INTEGER, IN pIdPortabilite INTEGER, IN pModele VARCHAR (100) )
 BEGIN
-    DECLARE pIdPortabilite INTEGER;
-    DECLARE pIdMarque INTEGER;
-    SET pIdMarque = 1;
-
-    IF pPortable = 'fixe'
-            {SET pIdPortabilite = 1;}
-    IF pPortable = 'portable'
-            {SET pIdPortabilite = 2;}
-
-    WHILE pMarque != (SELECT nomMarque FROM marques WHERE idMarque = pIdMarque) AND pIdMarque =< count(marques)
-        pIdMarque += 1;
-    IF pMarque != (SELECT nomMarque FROM marques WHERE idMarque = pIdMarque)
-        INSERT INTO marques
-            (idMarque,nomMarque)
-            VALUES
-            (pIdMarque,pMarque);
-
-
-    INSERT INTO consoles
-        (idPortabilite, idMarque, modele, annee, idSecurite)
-        VALUES 
-        (pIdPortabilite,pIdMarque,pModele,pAnnee,0);
-
+	INSERT INTO consoles                                    /* ajout de la console à la DB */
+        (idMarque, annee, idPortabilite, modele)
+        VALUES
+        (pIdMarque, pAnnee, pIdPortabilite, pModele)
 END
+
+CREATE PROCEDURE "DBA"."ajoutMarque"( in pMarque VARCHAR (100) )
+BEGIN
+	INSERT INTO marques         /* ajoute la marque à la DB */
+        (nomMarque)
+        VALUES 
+        (pMarque);
+END
+
+CREATE PROCEDURE "DBA"."getIdMarque"( in pMarque VARCHAR (100) )
+BEGIN
+	SELECT idMarque         /**/
+    FROM marques
+    WHERE nomMarque = pMarque
+END
+
+CREATE PROCEDURE "DBA"."verifExiste"( in pModele VARCHAR (100) )
+BEGIN
+	SELECT modele 
+    FROM consoles
+    WHERE modele = pModele
+END
+
+CREATE PROCEDURE "DBA"."verifMarque"( in pMarque VARCHAR (100) )
+BEGIN
+	SELECT nomMarque
+    FROM marques
+    WHERE nomMarque = pMarque
+END
+
 
 /* *********************************************** */
 
+CREATE SERVICE "ajoutMarque" TYPE 'Raw' AUTHORIZATION OFF USER "DBA" URL ON METHODS 'GET' AS call ajoutMarque(:pMarque);
 CREATE SERVICE "ajoutConsole" TYPE 'Raw' AUTHORIZATION OFF USER "DBA" URL ON METHODS 'GET' AS call ajoutConsole(:pMarque,:pAnnee,:pPortabilite,:pModele);
+CREATE SERVICE "idMarque" TYPE 'Raw' AUTHORIZATION OFF USER "DBA" URL ON METHODS 'GET' AS call getIdMarque(:pMarque);
+CREATE SERVICE "verifMarque" TYPE 'RAW' AUTHORIZATION OFF USER "DBA" URL ON METHODS 'GET' AS call verifMarque(:pMarque);
+CREATE SERVICE "verifExiste" TYPE 'Raw' AUTHORIZATION OFF USER "DBA" URL ON METHODS 'GET' AS call verifExiste(:pModele);
