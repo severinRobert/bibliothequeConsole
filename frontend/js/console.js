5a879c17c4f2b5f21e3e2acc6c75082add2bb289
ArthurBehets <64364135+ArthurBehets@users.noreply.github.com>
2020-05-10 16:14:49 +0200

    Create console.js

---
+++ frontend/js/console.js
@@ -0,0 +1,7 @@
+usestrict;
+document.addEventListener('DOMContentLoaded',initPage);
+
+function initPage (){
+    document.getElementById('formulaireConsole').addEventListener('submit',traitementFormulaire);
+    document.getElementById('submitComment').addEventListener('submit',addComment)
+}
