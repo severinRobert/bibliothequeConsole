017da3c6f2a8ad8c3aa0de562cb04742fa53b22c
EmilienPerremans <64018946+EmilienPerremans@users.noreply.github.com>
2020-05-12 12:34:38 +0200

    Update severinRobert-webservice-ajout.sql

--- backend/scripts-api-rest/severinRobert-webservice-ajout.sql
+++ backend/scripts-api-rest/severinRobert-webservice-ajout.sql
@@ -1,5 +1,6 @@
 /* auteur : Séverin Robert HE201811 */
 
+/*Procédure*/
 CREATE PROCEDURE "DBA"."ajoutConsole"( IN pIdMarque INTEGER, IN pAnnee INTEGER, IN pIdPortabilite INTEGER, IN pModele VARCHAR (100) )
 BEGIN
 	INSERT INTO consoles                                    /* ajout de la console à la DB */
@@ -7,6 +8,14 @@ BEGIN
         VALUES
         (pIdMarque, pAnnee, pIdPortabilite, pModele)
 END;
+
+/*Service Web*/
+
+CREATE SERVICE "ajoutMarque" TYPE 'Raw' AUTHORIZATION OFF USER "DBA" URL ON METHODS 'GET' AS call ajoutMarque(:pMarque);
+
+/***********************************************************************************************************************************/
+
+/*Procédure*/
 
 CREATE PROCEDURE "DBA"."ajoutMarque"( in pMarque VARCHAR (100) )      /* ajoute la marque à la DB */
 BEGIN
@@ -16,6 +25,14 @@ BEGIN
         (pMarque);
 END;
 
+/*Service Web*/
+
+CREATE SERVICE "ajoutConsole" TYPE 'Raw' AUTHORIZATION OFF USER "DBA" URL ON METHODS 'GET' AS call ajoutConsole(:pIdMarque,:pAnnee,:pIdPortabilite,:pModele);
+
+/***********************************************************************************************************************************/
+
+/*Procédure*/
+
 CREATE PROCEDURE "DBA"."getIdMarque"( in pMarque VARCHAR (100) )   /* trouve l'id de la marque encodée */
 BEGIN
     SELECT idMarque        
@@ -23,12 +40,26 @@ BEGIN
     WHERE nomMarque = pMarque
 END;
 
+/*Service Web*/
+
+CREATE SERVICE "idMarque" TYPE 'Raw' AUTHORIZATION OFF USER "DBA" URL ON METHODS 'GET' AS call getIdMarque(:pMarque);
+
+/***********************************************************************************************************************************/
+/*Procédure*/
+
 CREATE PROCEDURE "DBA"."verifExiste"( in pModele VARCHAR (100) )  /*  */
 BEGIN
 	SELECT modele 
     FROM consoles
     WHERE modele = pModele
 END;
+
+/*Web Service*/
+
+CREATE SERVICE "verifMarque" TYPE 'RAW' AUTHORIZATION OFF USER "DBA" URL ON METHODS 'GET' AS call verifMarque(:pMarque);
+
+/***********************************************************************************************************************************/
+/*Procédure*/
 
 CREATE PROCEDURE "DBA"."verifMarque"( in pMarque VARCHAR (100) )
 BEGIN
@@ -43,17 +74,18 @@ BEGIN
     FROM marques
 END;
 
+/*Web Service*/
+
+CREATE SERVICE "verifExiste" TYPE 'Raw' AUTHORIZATION OFF USER "DBA" URL ON METHODS 'GET' AS call verifExiste(:pModele);
+
+/***********************************************************************************************************************************/
+/*Procédure*/
 CREATE PROCEDURE "DBA"."tableauMarque"(  )
 BEGIN
 	SELECT nomMarque 
     FROM marques
 END;
 
-/* *********************************************** */
+/*Web Service*/
 
-CREATE SERVICE "ajoutMarque" TYPE 'Raw' AUTHORIZATION OFF USER "DBA" URL ON METHODS 'GET' AS call ajoutMarque(:pMarque);
-CREATE SERVICE "ajoutConsole" TYPE 'Raw' AUTHORIZATION OFF USER "DBA" URL ON METHODS 'GET' AS call ajoutConsole(:pIdMarque,:pAnnee,:pIdPortabilite,:pModele);
-CREATE SERVICE "idMarque" TYPE 'Raw' AUTHORIZATION OFF USER "DBA" URL ON METHODS 'GET' AS call getIdMarque(:pMarque);
-CREATE SERVICE "verifMarque" TYPE 'RAW' AUTHORIZATION OFF USER "DBA" URL ON METHODS 'GET' AS call verifMarque(:pMarque);
-CREATE SERVICE "verifExiste" TYPE 'Raw' AUTHORIZATION OFF USER "DBA" URL ON METHODS 'GET' AS call verifExiste(:pModele);
 CREATE SERVICE "tableauMarque" TYPE 'JSON' AUTHORIZATION OFF USER "DBA" URL ON METHODS 'GET' AS call tableauMarque();
