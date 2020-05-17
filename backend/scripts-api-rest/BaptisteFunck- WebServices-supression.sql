2738ab9b5e438173e760195fb1bd2002d1dd61f6
BaptisteFunck <64362486+BaptisteFunck@users.noreply.github.com>
2020-05-17 17:35:43 +0200

    Update BaptisteFunck- WebServices-supression.sql

--- backend/scripts-api-rest/BaptisteFunck- WebServices-supression.sql
+++ backend/scripts-api-rest/BaptisteFunck- WebServices-supression.sql
@@ -1,17 +1,27 @@
+/*Auteur: Baptiste Funck HE201830 */
+
+/*Procédure*/
 CREATE PROCEDURE "suppressionConsole" (in pModele VARCHAR(100))
 BEGIN 
     DELETE FROM "TableConsole"
     WHERE Modele = pModele
-END
+END;
+
+/*Service Web*/
 
 CREATE SERVICE "supressionConsole" TYPE 'Raw' AUTHORIZATION OFF USER "DBA" URL ON METHODS 'GET' AS call suppressionConsole(:pModele);
 
+/***********************************************************************************************************************************/
+
+/*Procédure*/
 
 CREATE PROCEDURE "verificationSecuritee" (in pModele VARCHAR (100))
 BEGIN 
     SELECT idsecurite
     FROM "TableConsole"
     WHERE Modele = pModele
-END
+END;
+
+/*Service Web*/
 
 CREATE SERVICE "verificationSecuritee" TYPE 'Raw' AUTHORIZATION OFF USER "DBA" URL ON METHODS 'GET' AS call verificationSecuritee(:pModele);
