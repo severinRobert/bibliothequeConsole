ba25ded29d3efb7471f8d892484d7852eb3d046c
BaptisteFunck <64362486+BaptisteFunck@users.noreply.github.com>
2020-05-17 15:48:30 +0200

    Add files via upload

--- frontend/js/BaptisteFunck-suppression-console.js
+++ frontend/js/BaptisteFunck-suppression-console.js
@@ -1,1 +1,19 @@
-"use strict";
+let rConsoleExiste = XMLHttpRequest();
+rConsoleExiste.open("get", "verifExiste?pModele=${pModele}" + "", true);
+rConsoleExiste.onload = () =>{
+
+        if(!rExiste.reponseText)     {
+                id("suppressionInfo").innerHTML += "La suppression de la console n'a pas pu se faire car la console n'existe pas. <br/>
+        
+        
+        }
+        elif((id"securite"= "1")    {
+                id(consoleSecurisee).innerHTML += " La suppression n'a pu se faire car la console fait partie des consoles vérifiées et sécurisées <br/>
+        }
+
+
+        else{
+           let rSuppressionConsole = XMLHttpRequest();
+           rSuppressionConsole.open()
+        }
+}
