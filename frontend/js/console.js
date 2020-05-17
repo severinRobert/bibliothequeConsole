42ce4c1ed9ef7c66f21a19d9f2062e7f19ab0995
ArthurBehets <64364135+ArthurBehets@users.noreply.github.com>
2020-05-17 15:39:00 +0200

    Update console.js

--- frontend/js/console.js
+++ frontend/js/console.js
@@ -3,11 +3,13 @@ document.addEventListener('DOMContentLoaded',initPage);
 
 function initPage (){
     id('formulaireConsole').addEventListener('submit',traitementFormulaire);
+    id('formulaireConsoleDeux').addEventListener('submit', traitementFormulaireDeux)
     id('submitComment').addEventListener('submit',addComment)
     id("ajoutConsole").addEventListener('submit',traitementAjoutConsole);
     ajoutMarqueChoix();
+    ajoutMarqueChoixDeux();
 }
 
 function id (idDom){
-	return document.getElementById(idDom)
+    return document.getElementById(idDom)
 }
