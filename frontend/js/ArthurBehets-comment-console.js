2949a73d369302ae6380d71ab40c10b1cd153e08
ArthurBehets <64364135+ArthurBehets@users.noreply.github.com>
2020-05-17 15:38:18 +0200

    Update ArthurBehets-comment-console.js

--- frontend/js/ArthurBehets-comment-console.js
+++ frontend/js/ArthurBehets-comment-console.js
@@ -35,3 +35,109 @@ function addComment(event){
     }
     newCommentXhr.send();
 }
+
+
+
+
+
+
+
+/***********************************************************************************************************
+
+ * Code copié de Emilien Perremans
+
+
+ */
+
+
+ function traitementFormulaireDeux (event){
+    event.preventDefault();
+    let form = event.target;
+    let pMarque = form.marqueDeux.value;
+    let pAnnee = form.anneeDeux.value;
+    let pPortabilite = form.portabiliteDeux.value;
+
+    if (pPortabilite == 'tous')
+    {
+        let xhr2 = new XMLHttpRequest();
+        xhr2.open('get' ,`tableConsoleHtmlMarque?pMarque=${pMarque}`,true );
+        xhr2.onload = () => { console.log(xhr2.responseText);
+            let rTableConsole = JSON.parse(xhr2.responseText);
+            let table = tableHtml(rTableConsole);
+            id("resultDeux").innerHTML = table;
+
+            if (pAnnee.length == 0) {
+                id("erreurDateDeux").innerHTML = "Champs date ignoré";
+            }
+        }
+        xhr2.send();
+    }
+
+    else
+    {
+        let xhr = new XMLHttpRequest();
+        xhr.open('get','tableConsoleHtml?pMarque=' + pMarque + '&pAnnee=' + pAnnee + '&pPortabilite=' + pPortabilite + '',true);
+        xhr.onload = () => { console.log(xhr)
+            let rTableConsole = JSON.parse(xhr.responseText);
+            if (rTableConsole.length != 0){
+                console.log(rTableConsole)
+                let tableau = tableHtml(rTableConsole);
+
+                id("resultDeux").innerHTML = tableau;
+                id("erreurDateDeux").innerHTML = "";
+            }
+
+            else {
+                let xhr1 = new XMLHttpRequest();
+                xhr1.open('get' ,`tableConsoleHtmlDate?pMarque=${pMarque}&pPortabilite=${pPortabilite}`,true );
+                xhr1.onload = () => { console.log(xhr1);
+                    rTableConsole = JSON.parse(xhr1.responseText);
+                    if (rTableConsole.length != 0){
+                        console.log(rTableConsole)
+                        let tableau = tableHtml(rTableConsole);
+
+                        id("resultDeux").innerHTML = tableau;
+                        id("erreurDateDeux").innerText = 'Champs date ignoré!';
+                    }
+                    else {
+                        id("erreurDateDeux").innerText = `La marque ${pMarque} n'as pas de consoles ${pPortabilite}` ;
+                    }
+
+                }
+                xhr1.send()
+            }
+        }
+        xhr.send()
+    }
+}
+
+ function tableHtml (array){
+    let tableau = "" ;
+
+    array.forEach (element=>{
+        if (!id(element.idConsole)){
+            tableau+=` <tr id="${element.idConsole}"> <td class="${element.nomMarque}">${element.nomMarque}</td>    <td id="${element.modele}">${element.modele}</td>     <td class="${element.annee}">${element.annee}</td>      <td class="${element.typePortabilite}">${element.typePortabilite}</td> </tr> `
+        }
+    });
+    return tableau;
+}
+
+
+
+/*******************************************************************************************************
+ *Code copié de Severin Robert
+ */
+
+function ajoutMarqueChoixDeux() {
+    let  tableauMarque = new XMLHttpRequest();
+    tableauMarque.open('get' ,"tableauMarque?",true );
+    tableauMarque.onload = () => {
+        let rTableMarque = JSON.parse(tableauMarque.responseText);
+        let tableHtmlMarque = "";
+        for(let i in rTableMarque)	{
+            tableHtmlMarque += `<option value="${rTableMarque[i].nomMarque}">${rTableMarque[i].nomMarque}</option>`		//creation des options de marque
+        }
+        id("marqueDeux").innerHTML = tableHtmlMarque;
+    }
+    tableauMarque.send();
+}
